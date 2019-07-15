d6d397671b9499a456982f9d0bdb601c8195033f
LIUST3 <stefan.liu@oocl.com>
2019-07-15 16:58:51 +0800

    finish test mock

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -1,32 +1,62 @@
 package cashregister;
 
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.mockito.Mockito.*;
 
 public class CashRegisterTest {
+    private ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
+    @BeforeEach
+    public void setup() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    private String systemOut() {
+        return outputStream.toString();
+    }
 
     @Test
     public void should_print_the_real_purchase_when_call_process() {
-        //given
-        //when
-        //then
+        Printer printer = new Printer();
+        CashRegister cashRegister = new CashRegister(printer);
+        Item item = new Item("Coco", 20.0);
+        Item[] items = new Item[]{item};
+        Purchase purchase = new Purchase(items);
+        cashRegister.process(purchase);
+        Assertions.assertEquals("Coco\t20.0\n",systemOut());
     }
 
     @Test
     public void should_print_the_stub_purchase_when_call_process() {
         //given
+        Printer printer = new Printer();
+        CashRegister cashRegister = new CashRegister(printer);
+        Purchase purchase = mock(Purchase.class);
         //when
+        when(purchase.asString()).thenReturn("Coco\t20\n");
+        cashRegister.process(purchase);
         //then
+        Assertions.assertEquals("Coco\t20\n",systemOut());
     }
 
     @Test
     public void should_verify_with_process_call_with_mockito() {
         //given
+        Printer printer = new Printer();
+        CashRegister cashRegister = new CashRegister(printer);
+        Purchase purchase = mock(Purchase.class);
         //when
+        when(purchase.asString()).thenReturn("Coco\t20\n");
+        cashRegister.process(purchase);
         //then
+        verify(purchase).asString();
     }
 
 }
